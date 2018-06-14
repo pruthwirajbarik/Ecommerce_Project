46e217930b1ffdc52c73160a6384fc53ab2a51ab
Pruthwiraj <Pruthwiraj@192.168.0.4>
2018-06-14 21:04:17 +0530

    First commit

---
+++ Ecommerce_git_Jenkins/src/main/java/Ecommerce_git_Jenkins/Ecommerce_git_Jenkins/App.java
@@ -0,0 +1,13 @@
+package Ecommerce_git_Jenkins.Ecommerce_git_Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ecommerce_git_Jenkins/src/test/java/test/Testclass.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Testclass {
+	@Test
+	public void build()
+	{
+		System.out.println("First build test");
+	}
+
+}
